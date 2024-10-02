5c10aa34030b70ebc7ee7a39f8ac210bf1920717
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2024-10-02 20:09:09 +0000

    feat: add new fancy loading indicator

---
+++ src/lib/components/ui/loading/SpinningDotsIndicator.svelte
@@ -0,0 +1,121 @@
+<script lang="ts">
+  import clsx from 'clsx';
+  import { onDestroy, onMount } from 'svelte';
+  import settings from '$lib/stores/settings';
+
+  export let delay: number | undefined = 0;
+
+  const dotClass = clsx(['absolute', 'w-4 h-4', 'rounded-full']);
+
+  let showIndicator = !delay;
+  let timeout: any;
+
+  onMount(() => {
+    if (!showIndicator) {
+      timeout = setTimeout(() => {
+        showIndicator = true;
+      }, delay);
+    }
+  });
+
+  onDestroy(() => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+  });
+</script>
+
+{#if showIndicator && !$settings.forceReducedMotion}
+  <div
+    class="backdrop flex justify-center items-center fixed top-0 left-0 right-0 bottom-0 z-50 bg-gray-100/50 dark:bg-gray-900/50 backdrop-blur-sm"
+  >
+    <div class="loading-indicator relative w-48 h-48">
+      <div class="dot-1 {dotClass} bg-fuchsia-500 shadow-[0px_0px_16px_2px_rgba(217,_70,_239,_1)]"></div>
+      <div class="dot-2 {dotClass} bg-fuchsia-700 shadow-[0px_0px_16px_2px_rgba(162,_28,_175,_1)]"></div>
+      <div class="dot-3 {dotClass} bg-violet-700 shadow-[0px_0px_16px_2px_rgba(109,_40,_217,_1)]"></div>
+      <div class="dot-4 {dotClass} bg-blue-700 shadow-[0px_0px_16px_2px_rgba(29,_78,_216,_1)]"></div>
+      <div class="dot-5 {dotClass} bg-blue-500 shadow-[0px_0px_16px_2px_rgba(59,_130,_246,_1)]"></div>
+      <div class="dot-6 {dotClass} bg-cyan-500 shadow-[0px_0px_16px_2px_rgba(6,_182,_212,_1)]"></div>
+      <div class="dot-7 {dotClass} bg-cyan-300 shadow-[0px_0px_16px_2px_rgba(103,_232,_249,_1)]"></div>
+    </div>
+  </div>
+{/if}
+
+<style lang="postcss">
+  .loading-indicator {
+    animation: dot-spin 2s linear infinite;
+  }
+
+  .dot-1 {
+    top: calc(0rem - 0.5rem);
+    left: calc(6rem - 0.5rem);
+    animation: dot-scale 1.6s linear infinite;
+  }
+
+  .dot-2 {
+    top: calc(6rem - (sin(135deg) * 6rem) - 0.5rem);
+    left: calc(6rem + (cos(135deg) * 6rem) - 0.5rem);
+    animation: dot-scale 1.6s 0.2s linear infinite;
+  }
+
+  .dot-3 {
+    top: calc(6rem - 0.5rem);
+    left: calc(0rem - 0.5rem);
+    animation: dot-scale 1.6s 0.4s linear infinite;
+  }
+
+  .dot-4 {
+    top: calc(6rem - (sin(225deg) * 6rem) - 0.5rem);
+    left: calc(6rem + (cos(225deg) * 6rem) - 0.5rem);
+    animation: dot-scale 1.6s 0.6s linear infinite;
+  }
+
+  .dot-5 {
+    top: calc(12rem - 0.5rem);
+    left: calc(6rem - 0.5rem);
+    animation: dot-scale 1.6s 0.8s linear infinite;
+  }
+
+  .dot-6 {
+    top: calc(6rem - (sin(315deg) * 6rem) - 0.5rem);
+    left: calc(6rem + (cos(315deg) * 6rem) - 0.5rem);
+    animation: dot-scale 1.6s 1s linear infinite;
+  }
+
+  .dot-7 {
+    top: calc(6rem - 0.5rem);
+    left: calc(12rem - 0.5rem);
+    animation: dot-scale 1.6s 1.2s linear infinite;
+  }
+
+  @keyframes dot-scale {
+    0% {
+      transform: scale(1);
+    }
+    25% {
+      transform: scale(2.25);
+    }
+    50%,
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  @keyframes dot-spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    25% {
+      transform: rotate(90deg);
+    }
+    50% {
+      transform: rotate(180deg);
+    }
+    75% {
+      transform: rotate(270deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
