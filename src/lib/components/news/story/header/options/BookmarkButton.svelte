9a88ea0a16894bcd00b124ea7e750eea690f07da
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2025-01-29 09:35:21 +0000

    feat(news): show in-app notification when bookmark is added/removed

--- src/lib/components/news/story/header/options/BookmarkButton.svelte
+++ src/lib/components/news/story/header/options/BookmarkButton.svelte
@@ -2,8 +2,10 @@
   import Button from '$lib/components/ui/controls/Button.svelte';
   import BookmarkIcon from '$lib/components/ui/icons/outline/BookmarkIcon.svelte';
   import BookmarkSlashIcon from '$lib/components/ui/icons/outline/BookmarkSlashIcon.svelte';
+  import { NOTIFICATION_BOOKMARK_TIMEOUT } from '$lib/configs/client';
   import type { Story } from '$lib/models/story';
   import bookmarks from '$lib/stores/bookmarks';
+  import notifications from '$lib/stores/notifications';
 
   interface Props {
     story: Story;
@@ -15,11 +17,19 @@
 
   function handleAddToBookmarksClick() {
     bookmarks.add(story);
+    notifications.notify('Lesezeichen hinzugefügt', 'Der Artikel wurde zu deinen Lesezeichen hinzugefügt.', {
+      forceAppNotification: true,
+      timeoutInMs: NOTIFICATION_BOOKMARK_TIMEOUT,
+    });
     onClose?.();
   }
 
   function handleRemoveFromBookmarksClick() {
     bookmarks.remove(story);
+    notifications.notify('Lesezeichen entfernt', 'Der Artikel wurde von deinen Lesezeichen entfernt.', {
+      forceAppNotification: true,
+      timeoutInMs: NOTIFICATION_BOOKMARK_TIMEOUT,
+    });
     onClose?.();
   }
 </script>
