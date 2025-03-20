2baa79ca3f016e92e5c7d8149be49eb09b2b94b8
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2025-03-20 20:21:35 +0000

    fix(StoryImageViewer): hide info button on touch devices

--- src/lib/components/news/story/content/StoryImageViewer.svelte
+++ src/lib/components/news/story/content/StoryImageViewer.svelte
@@ -280,7 +280,7 @@
     {/if}
   {/if}
   <Popover
-    containerClass="viewer-button bottom-2 right-2 {viewerButtonClass}"
+    containerClass="viewer-button viewer-button-touch bottom-2 right-2 {viewerButtonClass}"
     placement="top-end"
     openOnHover
     delay={{ open: 250 }}
@@ -342,6 +342,12 @@
         background-color: theme('colors.gray.500');
         opacity: 0.9;
       }
+    }
+  }
+
+  :global(.viewer-button-touch) {
+    @media screen and (pointer: coarse) {
+      display: none;
     }
   }
 
