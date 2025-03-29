f01f4801a36e944ffdd4a1bbefdcc70561c5d8a2
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2025-03-29 22:44:29 +0000

    fix(story): revert header sticky handling

--- src/lib/components/news/story/Story.svelte
+++ src/lib/components/news/story/Story.svelte
@@ -21,7 +21,7 @@
   const subscriptions: Array<Subscription> = [];
 
   const headerClass = `
-    flex flex-row items-center gap-3 sticky top-[47px] sm:top-[53px] z-10
+    flex flex-row items-center gap-3 top-[47px] sm:top-[53px]
     ${defaultPadding}
     text-gray-800 dark:text-gray-300 hover:text-blue-700 dark:hover:text-blue-500
     border-gray-200 dark:border-gray-700
@@ -34,7 +34,9 @@
   let headerRef: StoryHeader | undefined = $state();
   let showContentInitial = false;
   let showContent = $state(false);
-  let headerClassOpen = $derived(clsx([showContent && 'mb-[-2px] border-solid border-b-2 bg-white dark:bg-gray-900']));
+  let headerClassOpen = $derived(
+    clsx([showContent && 'sticky z-10 mb-[-2px] border-solid border-b-2 bg-white dark:bg-gray-900']),
+  );
 
   $effect(() => {
     handleContentViewCollapse(showContent);
