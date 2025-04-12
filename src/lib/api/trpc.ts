9db6b309a2b9cb3f4f8666211289247308db87fe
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2025-04-12 13:39:57 +0000

    fix(api): update createTRPC to assign and return the client instance

--- src/lib/api/trpc.ts
+++ src/lib/api/trpc.ts
@@ -1,18 +1,20 @@
 import type { AppRouter } from '$lib/backend/trpc/router';
 import { createTRPCClient, httpLink, type TRPCClient } from '@trpc/client';
 
-const trpc: TRPCClient<AppRouter> | null = null;
+let trpc: TRPCClient<AppRouter> | null = null;
 
 export function createTRPC(origin?: string): TRPCClient<AppRouter> {
   if (trpc) {
     return trpc;
   }
 
-  return createTRPCClient<AppRouter>({
+  trpc = createTRPCClient<AppRouter>({
     links: [
       httpLink({
         url: `${origin ?? ''}/api/trpc`,
       }),
     ],
   });
+
+  return trpc;
 }
