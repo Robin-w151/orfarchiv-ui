f28ef2a00a149662073a4a8ef07f1d6057cdc015
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2025-04-18 15:17:19 +0000

    fix(story): add error message if ai summary cannot be generated

--- src/lib/components/news/story/content/StoryAiSummary.svelte
+++ src/lib/components/news/story/content/StoryAiSummary.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+  import AlertBox from '$lib/components/shared/content/AlertBox.svelte';
   import Modal from '$lib/components/shared/content/Modal.svelte';
   import { StoryContent } from '$lib/models/story';
-  import notifications from '$lib/stores/notifications';
   import settings from '$lib/stores/settings';
   import { logger } from '$lib/utils/logger';
   import { GoogleGenAI, type GenerateContentResponse } from '@google/genai';
   import SvelteMarkdown from '@humanspeak/svelte-markdown';
+  import { ExclamationCircle } from '@steeze-ui/heroicons';
   import { onDestroy, onMount } from 'svelte';
   import { get } from 'svelte/store';
   import StoryAiSummarySkeleton from './StoryAiSummarySkeleton.svelte';
@@ -20,9 +21,11 @@
   let aiSummary = $state<string>('');
   let aiSummaryStream: AsyncGenerator<GenerateContentResponse> | undefined;
   let aiSummaryLoading = $state(false);
+  let aiSummaryError = $state<{ title: string; message: string } | undefined>(undefined);
 
   onMount(async () => {
     if (!storyContent?.contentText) {
+      onClose?.();
       return;
     }
 
@@ -30,9 +33,10 @@
     const apiKey = get(settings).geminiApiKey;
     if (!apiKey) {
       logger.warn('No Gemini API Key found');
-      notifications.notify('Kein API-Key hinterlegt', 'Es ist kein API-Key für die AI Zusammenfassung hinterlegt.', {
-        forceAppNotification: true,
-      });
+      aiSummaryError = {
+        title: 'Kein API-Key hinterlegt',
+        message: 'Es ist kein API-Key für die AI Zusammenfassung hinterlegt.',
+      };
       return;
     }
 
@@ -61,13 +65,11 @@
       }
     } catch (error) {
       logger.warn(`Error: ${error}`);
-      notifications.notify(
-        'Fehler beim Generieren der KI-Zusammenfassung',
-        'Es ist ein Fehler beim Generieren der KI-Zusammenfassung aufgetreten.',
-        {
-          forceAppNotification: true,
-        },
-      );
+      aiSummaryError = {
+        title: 'Ein Fehler ist aufgetreten',
+        message:
+          'Beim Generieren der KI-Zusammenfassung ist ein Fehler aufgetreten. Bitte versuchen Sie es später erneut.',
+      };
     }
 
     logger.debugGroup('ai-summary', [['ai-summary', aiSummary]], true);
@@ -92,17 +94,21 @@
 </script>
 
 <Modal modalClass="w-full h-full sm:h-3/5 lg:h-3/5" label="KI-Zusammenfassung" onClose={handleAiSummaryClose}>
-  <article class="story-content" data-testid="ai-summary">
-    <div class="byline">
-      <p>
-        Erstellt mit KI (Modell: <a href="https://duckduckgo.com/?q=ai+model+{$settings.aiModel}" target="_blank"
-          >{$settings.aiModel}</a
-        >)
-      </p>
-    </div>
-    <SvelteMarkdown source={aiSummary}></SvelteMarkdown>
-  </article>
-  {#if aiSummaryLoading}
-    <StoryAiSummarySkeleton />
+  {#if aiSummaryError}
+    <AlertBox title={aiSummaryError.title} message={aiSummaryError.message} icon={ExclamationCircle} />
+  {:else}
+    <article class="story-content" data-testid="ai-summary">
+      <div class="byline">
+        <p>
+          Erstellt mit KI (Modell: <a href="https://duckduckgo.com/?q=ai+model+{$settings.aiModel}" target="_blank"
+            >{$settings.aiModel}</a
+          >)
+        </p>
+      </div>
+      <SvelteMarkdown source={aiSummary}></SvelteMarkdown>
+    </article>
+    {#if aiSummaryLoading}
+      <StoryAiSummarySkeleton />
+    {/if}
   {/if}
 </Modal>
