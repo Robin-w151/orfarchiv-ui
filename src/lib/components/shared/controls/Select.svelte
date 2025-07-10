fbbef11e9923ad910b09e3ddf45c94eaaa40ce34
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2025-07-10 17:34:28 +0000

    fix(select): improve select element styles

--- src/lib/components/shared/controls/Select.svelte
+++ src/lib/components/shared/controls/Select.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { ChevronDown } from '@steeze-ui/heroicons';
+  import { Icon } from '@steeze-ui/svelte-icon';
   import type { Snippet } from 'svelte';
 
   interface Props {
@@ -11,12 +13,15 @@
 
   let { id, value = $bindable(''), placeholder, onchange, children }: Props = $props();
 
+  const selectContainerClass = ['flex items-center relative w-full flex-1'];
   const selectClass = [
     'pr-12 w-full',
     'text-gray-800 dark:text-gray-300 bg-gray-100 dark:bg-gray-800 focus:bg-white dark:focus:bg-gray-900',
     'rounded-md',
     'transition',
+    'bg-none',
   ];
+  const selectIconClass = ['absolute right-3', 'size-6', 'text-gray-700 dark:text-gray-300', 'pointer-events-none'];
 
   let selectRef: HTMLSelectElement | undefined = $state();
 
@@ -32,6 +37,9 @@
   }
 </script>
 
-<select class={selectClass} {id} {placeholder} bind:value bind:this={selectRef} onkeydown={handleKeydown}>
-  {@render children?.()}
-</select>
+<div class={selectContainerClass}>
+  <select class={selectClass} {id} {placeholder} bind:value bind:this={selectRef} onkeydown={handleKeydown}>
+    {@render children?.()}
+  </select>
+  <Icon class={selectIconClass} src={ChevronDown} />
+</div>
