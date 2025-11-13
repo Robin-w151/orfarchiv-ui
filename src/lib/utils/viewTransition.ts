76678d9be7cc36244cb3cf828fe0244a922a5b17
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2025-11-13 22:02:15 +0000

    fix(settings): prevent appearance settings skips

--- src/lib/utils/viewTransition.ts
+++ src/lib/utils/viewTransition.ts
@@ -1,7 +1,7 @@
 import { logger } from './logger';
 import { isViewTransitionAvailable } from './support';
 
-let isViewTransitionInProgress = false;
+let _isViewTransitionInProgress = false;
 
 export function runViewTransition(
   fn: () => void | Promise<void>,
@@ -9,18 +9,22 @@ export function runViewTransition(
 ): void {
   if (isViewTransitionAvailable() && !useReducedMotion) {
     logger.debug('view-transition: starting');
-    if (isViewTransitionInProgress) {
+    if (_isViewTransitionInProgress) {
       logger.debug('view-transition: already in progress');
       return;
     }
-    isViewTransitionInProgress = true;
+    _isViewTransitionInProgress = true;
     const transition = document.startViewTransition(fn);
     transition.finished.then(() => {
-      isViewTransitionInProgress = false;
+      _isViewTransitionInProgress = false;
       logger.debug('view-transition: finished');
     });
   } else {
     logger.debug('view-transition: not available');
     fn();
   }
+}
+
+export function isViewTransitionInProgress(): boolean {
+  return _isViewTransitionInProgress;
 }
