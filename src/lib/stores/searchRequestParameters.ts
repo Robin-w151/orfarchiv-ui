3441482ffdcc9fe651f7fcf398258f634ec17cc7
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2026-04-08 15:06:52 +0000

    fix(search): fix date comparison

--- src/lib/stores/searchRequestParameters.ts
+++ src/lib/stores/searchRequestParameters.ts
@@ -1,4 +1,5 @@
 import type { SearchRequestParameters } from '$lib/models/searchRequest';
+import { DateTime } from 'luxon';
 import { BehaviorSubject, combineLatestWith, distinctUntilChanged, map } from 'rxjs';
 import searchFilter, { type SearchFilterStoreProps } from './searchFilter';
 import settings from './settings';
@@ -7,9 +8,19 @@ function searchFilterStorePropsEqual(p1?: SearchFilterStoreProps, p2?: SearchFil
   return (
     p1?.textFilter === p2?.textFilter &&
     p1?.tag === p2?.tag &&
-    p1?.dateFilter?.from === p2?.dateFilter?.from &&
-    p1?.dateFilter?.to === p2?.dateFilter?.to
+    datesEqual(p1?.dateFilter?.from, p2?.dateFilter?.from) &&
+    datesEqual(p1?.dateFilter?.to, p2?.dateFilter?.to)
   );
+}
+
+function datesEqual(d1?: DateTime, d2?: DateTime): boolean {
+  if (!d1 && !d2) {
+    return true;
+  }
+  if (!d1 || !d2) {
+    return false;
+  }
+  return d1.equals(d2);
 }
 
 const initialState = {};
